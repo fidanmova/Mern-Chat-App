a484702d6da2ccce63ed5b354d1bdf8c744c7141
FeeMowa <lyonmowa@gmail.com>
2022-11-10 16:51:26 +0100

    npm i

--- server/config/generateToken.js
+++
@@ -1,9 +0,0 @@
-// install to the root "npm i jsonwebtoken"
-const jwt = require("jsonwebtoken");
-
-const generateToken = (id) => {
-  return jwt.sign({ id }, process.env.JWT_SECRET, {
-    expiresIn: "30d",
-  });
-};
-module.exports = generateToken;
